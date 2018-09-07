b641e58df85965f6ec405a33a34f476d82df67da
hellocz <chenzhihn84@163.com>
2018-09-07 13:09:01 +0800

    Update newfrontendAction.class.php

--- app/Lib/Action/newfrontendAction.class.php
+++ app/Lib/Action/newfrontendAction.class.php
@@ -128,7 +128,7 @@ class newfrontendAction extends baseAction {
         include_once LIB_PATH . 'Pinlib/taobao/TopSdk.php';
         $c = new TopClient;
         $c->appkey = "23232602";
-        $c->secretKey = "a91ec4b0a09a93dd2c9e85d88665ef26";
+        $c->secretKey = "xxxxxxxxxxxxxxxxxx";
         $req = new TbkDgItemCouponGetRequest;
         $req->setAdzoneId("14718353");
        // $req->setPlatform(1);
